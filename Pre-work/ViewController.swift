9272507511580a901bc7ed5fcf212bdf43823e4e
saminmalik <samin.malik@stonybrook.edu>
2021-02-10 22:19:49 -0500

    Initial Commit

---
+++ Pre-work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pre-work
+//
+//  Created by Samin Malik on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
